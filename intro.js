97af8cc9c459f47f22ed4e53cb46d5c7b62b33cb
ljs06050 <109151173+ljs06050@users.noreply.github.com>
2025-06-02 16:44:22 +0900

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,13 +1,16 @@
 function intro() {
+    push();
     background(0);
     textAlign(CENTER, CENTER);
-    textSize(50);
+    textSize(100);
     fill(255);
     text("intro", width / 2, height / 2);
+    textSize(50);
     text("press ENTER to start", width / 2, 3 * height / 4);
     if (keyIsPressed) {
         if (keyCode == ENTER) {
             return 1;
         }
     }
+    pop();
 }
